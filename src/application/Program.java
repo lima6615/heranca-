fef0a4baf7008b1bb17c51aca2c746201df46e48
lima6615 <leo-queiroz1@outlook.com>
2021-07-07 13:15:25 -0300

    Criação de duas classe Account e BusinessAccount

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,15 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.BusinessAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		BusinessAccount account = new BusinessAccount();
 		
 		
 		
